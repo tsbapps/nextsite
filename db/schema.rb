955345355b2f43319d6eed4ab8322d6368db2b3a
tsbapps <gfinnie@telecomservicebureau>
2014-08-27 10:27:55 -0400

    added user_id to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822151534) do
+ActiveRecord::Schema.define(version: 20140827142127) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: true do |t|
     t.integer  "userid"
@@ -19,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140822151534) do
     t.datetime "date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
